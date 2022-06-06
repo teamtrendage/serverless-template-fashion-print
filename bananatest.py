73abae3ef8264b51d90bb67eab21e67b2b3d38f4
IamUmairMughal <>
2022-06-06 15:24:22 +0500

    Update src/app.py & added bananatest.py

---
+++ bananatest.py
@@ -0,0 +1,61 @@
+import banana_dev as banana
+import argparse
+import base64
+import json
+import cv2
+import numpy as np
+
+def main(args, api_key, model_key):
+    threshold = args.threshold
+    threshold = str(threshold).encode()
+    threshold = base64.b64encode(threshold)
+    padding = args.padding
+    padding = str(padding).encode()
+    padding = base64.b64encode(padding)
+ 
+    path = args.img_path
+    img = cv2.imread(path)
+    _, img_encoded = cv2.imencode('.png', img)
+    img_encoded = base64.b64encode(img_encoded).decode()
+
+    # send http request with image and receive response
+    model_inputs = {
+        'data':  img_encoded,
+        'threshold': threshold.decode('utf-8'),
+        'padding': padding.decode('utf-8'),
+    }    
+
+    response = banana.run(api_key, model_key, model_inputs)
+    
+    response = json.loads(response.text)
+
+    w,h = response["size"]
+
+    inpainted_img = response["inpainted_img"]
+    inpainted_img = base64.b64decode(inpainted_img)
+    inpainted_img = np.frombuffer(inpainted_img, np.uint8)
+    inpainted_img = inpainted_img.reshape(h,w,3)
+
+    cv2.imwrite("received_inpainted.png",inpainted_img)
+
+    bbox_img = response["bbox_img"]
+    bbox_img = base64.b64decode(bbox_img)
+    bbox_img = np.frombuffer(bbox_img, np.uint8)
+    bbox_img = bbox_img.reshape(h,w,3)
+    cv2.imwrite("received_bbox.png",bbox_img)
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('img_path', help="path of the image")
+    parser.add_argument('--threshold', type=float, help='confidence threshold',
+                        default=0.7)
+    parser.add_argument('--padding',type=int, 
+                        help='Extra pixels to padd in bounding-box',
+                        default=20)
+    args = parser.parse_args()
+
+    api_key = "777c010a-7781-4e14-be0e-dc84d9d3e2ee" # "YOUR_API_KEY"
+    model_key = "822d4fae-4e80-4845-a219-c59794e1d34c" # "YOUR_MODEL_KEY"
+
+    
+    main(args, api_key, model_key)
--- src/app.py
+++ src/app.py
@@ -7,13 +7,13 @@ import numpy as np
 import torch
 from flask import Flask, Response, jsonify, request
 from PIL import Image
-
+import json
 from warmup import load_model
 from run import run_model
 
 # do the warmup step globally, to have a reuseable model instance
 print('Loading Model....')
-device = 'cuda'
+device = 'cpu'
 model = load_model(device)
 print('Model Loaded!')
 
@@ -34,25 +34,33 @@ def healthcheck(request):
 @app.route('/api/test', methods=['POST'])
 def test():
     r = request
-    # convert string of image data to uint8
-    threshold = r.headers['threshold']
+    # Reading request json 
+    req = json.loads(r.json)
+    
+    # extracting and decoding threshold, padding 
+    threshold = req['threshold']
     threshold = base64.b64decode(threshold)
     threshold = float(threshold)
-    padding = r.headers['padding']
+    padding = req['padding']
     padding = base64.b64decode(padding)
     padding = int(padding)
-    nparr = np.fromstring(r.data, np.uint8)
-    # decode image
+    nparr = np.fromstring(req['data'], np.uint8)
+
+    # extracting and decoding image    
+    nparr = base64.b64decode(nparr)
+    nparr = np.frombuffer(nparr, dtype=np.uint8)
+    
     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
     img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
     
     img = Image.fromarray(img)
     w,h = img.size
+    
+    # Predicting    
     inpainted_img, bbox_img = run_model(model, img, threshold, padding)
     
     enc_inpaint = base64.b64encode(inpainted_img).decode('utf-8')
     enc_bbox_img = base64.b64encode(bbox_img).decode('utf-8')
-    # return Response(response=response_pickled, status=200, mimetype="application/json")
     return jsonify({
                 'msg': 'success', 
                 'size': [w, h], 
